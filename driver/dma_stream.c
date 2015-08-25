666759e33615e5112166a6153b98fa68de5741a0
Yichao Yu <yyc1992@gmail.com>
2015-08-25 17:00:29 -0400

    [driver] add slave thread

--- driver/dma_stream.c
+++ driver/dma_stream.c
@@ -22,9 +22,24 @@
 #include "dma_stream.h"
 
 #include <linux/amba/xilinx_dma.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
 
 static struct dma_chan *dma_stream_tx = NULL;
 static struct dma_chan *dma_stream_rx = NULL;
+static struct task_struct *slave_thread = NULL;
+
+static int
+knacs_dma_stream_slave(void *data)
+{
+    int counter = 0;
+    while (!kthread_should_stop()) {
+        if (counter++ % 300 == 10)
+            pr_info("DMA slave: %d\n", counter);
+        msleep(200);
+    }
+    return 0;
+}
 
 static int
 knacs_dma_stream_probe(struct platform_device *pdev)
@@ -48,11 +63,25 @@ knacs_dma_stream_probe(struct platform_device *pdev)
         pr_alert("Requesting Rx channel failed\n");
         goto free_tx;
     }
+    slave_thread = kthread_run(knacs_dma_stream_slave, NULL, "%s-%s",
+                               dma_chan_name(dma_stream_tx),
+                               dma_chan_name(dma_stream_rx));
+    if (IS_ERR(slave_thread)) {
+        err = PTR_ERR(slave_thread);
+        pr_alert("Failed to create thread\n");
+        goto free_thread;
+    }
+
+    /* srcbuf and dstbuf are allocated by the thread itself */
+    get_task_struct(slave_thread);
 
     pr_info("dma streams created.\n");
 
     return 0;
 
+free_thread:
+    slave_thread = NULL;
+    dma_release_channel(dma_stream_rx);
 free_tx:
     dma_stream_rx = NULL;
     dma_release_channel(dma_stream_tx);
@@ -64,13 +93,18 @@ clear_tx:
 static int
 knacs_dma_stream_remove(struct platform_device *pdev)
 {
+    if (slave_thread) {
+        kthread_stop(slave_thread);
+        put_task_struct(slave_thread);
+        slave_thread = NULL;
+    }
     if (dma_stream_rx) {
-        dma_stream_rx = NULL;
         dma_release_channel(dma_stream_rx);
+        dma_stream_rx = NULL;
     }
     if (dma_stream_tx) {
-        dma_stream_tx = NULL;
         dma_release_channel(dma_stream_tx);
+        dma_stream_tx = NULL;
     }
     return 0;
 }
